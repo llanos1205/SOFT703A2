5748a8315a34ae525d56119b533dcc5bd8d67615
llanos1205 <llanos1205@gmail.com>
2023-11-10 23:44:49 +1300

    Added missing bg color change

--- SOFT703A2.WebApp/wwwroot/js/MarketPlace/Index.js
+++ SOFT703A2.WebApp/wwwroot/js/MarketPlace/Index.js
@@ -20,14 +20,14 @@
                 
                 var card = '<div class="col-md-4 mb-4">' +
                     '<div class="card" style="width: 250px; height: 300px;">' +
-                    '<a href='+detailUrlWithId+'>' +
+                    '<a href=' + detailUrlWithId + '>' +
                     '<img src="' + product.Photo + '" class="card-img-top" alt="Product Image" style="height: 190px;">' +
                     '</a>' +
                     '<div class="card-body">' +
                     '<h6 class="card-title">' + product.Name + ' - $' + product.Price + '</h6>' +
                     '<h7 class="card-title">' + product.Category.Name + '</h7>' +
                     '</div>' +
-                    '<a href="javascript:void(0);" class="btn btn-primary addFromCatalog" data-product-id="' + product.Id + '">Add</a>' +
+                    '<a href="javascript:void(0);" class="btn btn-primary addFromCatalog bg-green" data-product-id="' + product.Id + '">Add</a>' +
                     '</div>' +
                     '</div>';
                 catalogContainer.append(card);
